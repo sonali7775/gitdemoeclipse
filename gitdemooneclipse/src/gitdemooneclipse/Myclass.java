359f211c44c0d1987cd10687948f4425691b82e2
sonali7775 <inglesonali009@gamil.com>
2021-06-16 20:32:30 +0530

    committing first time

---
+++ gitdemooneclipse/src/gitdemooneclipse/Myclass.java
@@ -0,0 +1,10 @@
+package gitdemooneclipse;
+
+public class Myclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.println("Hello");
+	}
+
+}
